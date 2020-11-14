c00e415f1851534c3152fb69c320dadc7464912f
ashsvis <ashsvis@gmail.com>
2020-11-14 16:28:58 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace LissajouFigures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingPlace = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingPlace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawingPlace
+            // 
+            this.drawingPlace.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.drawingPlace.Location = new System.Drawing.Point(13, 40);
+            this.drawingPlace.Name = "drawingPlace";
+            this.drawingPlace.Size = new System.Drawing.Size(908, 467);
+            this.drawingPlace.TabIndex = 0;
+            this.drawingPlace.TabStop = false;
+            this.drawingPlace.Click += new System.EventHandler(this.drawingPlace_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.drawingPlace);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingPlace)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawingPlace;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LissajouFigures
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Это PictureBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void drawingPlace_Click(object sender, EventArgs e)
+        {
+            var pointsList = new List<PointF>();
+            var t_max = 10.0;
+            var x_angle = 0.0;
+            var y_angle = 0.0;
+            var x_freq = 1.0;
+            var y_freq = 2.0;
+            var x_amp = 100.0;
+            var y_amp = 100.0;
+            var screen_width_center = this.Width / 2;
+            var screen_height_center = this.Height / 2;
+            var blackPen = Pens.Black;
+            if (checkBox1.Checked == false)
+            {
+                for (var t = 0.0; t <= t_max; t += Math.PI / 360)
+                {
+                    //Сразу рисует 
+                    var x = (screen_width_center + Convert.ToSingle(x_amp * Math.Sin(x_freq * t + x_angle)));
+                    var y = (screen_height_center + Convert.ToSingle(y_amp * Math.Sin(y_freq * t + y_angle)));
+                    pointsList.Add(new PointF(x, y));
+                }
+                Bitmap plane = new Bitmap(drawingPlace.Width, drawingPlace.Height);
+                var drawArea = Graphics.FromImage(plane);
+                drawArea.DrawLines(blackPen, pointsList.ToArray());
+                drawingPlace.Image = plane;
+                pointsList.Clear();
+            }
+            else
+            {
+                for (var t = 0.0; t <= t_max; t += Math.PI / 360)
+                {
+                    //Рисует постепенно 
+                    var x = (screen_width_center + Convert.ToSingle(x_amp * Math.Sin(x_freq * t + x_angle)));
+                    var y = (screen_height_center + Convert.ToSingle(y_amp * Math.Sin(y_freq * t + y_angle)));
+                    pointsList.Add(new PointF(x, y));
+                    var arr = pointsList.ToArray();
+                    if (arr.Length > 1)
+                    {
+                        using (Bitmap plane = new Bitmap(drawingPlace.Width, drawingPlace.Height))
+                        {
+                            using (var drawArea = Graphics.FromImage(plane))
+                            {
+                                drawArea.DrawLines(blackPen, arr);
+                            }
+                            drawingPlace.Image = plane;
+                            drawingPlace.Refresh();
+                        }
+                    }
+                }
+                pointsList.Clear();
+            }
+        }
+    }
+}
